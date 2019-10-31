a6511ea7b376cb0e10fc0941efb537e371a6a081
masterbashe <bashe.athini@gmail.com>
2019-10-31 05:32:35 +0200

    dummy

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/foundation.dart';
-
+import 'dart:io';
 class Place{
   final String id;
   final String title;
-  final ... location;
-  final ... image;
+  final PlaceLocation location;
+  final File image;
 
   Place({@required this.id, @required this.title, @required this.location, @required this.image});
+}
+class PlaceLocation{
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  PlaceLocation({@required this.latitude, @required this.longitude, this.address});
 }
