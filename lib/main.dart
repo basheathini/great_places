559d3f9b3ec75f700bc6b83a7d79ce9f992cb7c1
masterbashe <bashe.athini@gmail.com>
2019-10-31 05:32:35 +0200

    dummy

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'views/places_list_screen.dart';
+import 'package:provider/provider.dart';
+import 'providers/great_places.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,14 +9,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great places',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.lightGreen
+    return ChangeNotifierProvider.value(
+      value: GreatPlace(),
+      child: MaterialApp(
+        title: 'Great places',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            accentColor: Colors.lightGreen
+        ),
+        home: PlacesListScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: PlacesListScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
